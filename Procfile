e73d2751550e7d6e2038b2fe8892bef593340fd9
achan24 <58598070+achan24@users.noreply.github.com>
2025-04-05 11:21:41 +0100

    refreshed files - sample linux app

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: cd mysite && gunicorn --bind 127.0.0.1:8000 mysite.wsgi:application
--- mysite/mysite/tests.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase, Client
-from django.urls import reverse
-
-class HomePageTest(TestCase):
-    def test_home_page_returns_correct_response(self):
-        client = Client()
-        response = client.get('/')
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "Hello, AWS Elastic Beanstalk!")
